6d7d8d29d75c8596fa7a02e06286ad4033718066
oxygensend <szymon19314@wp.pl>
2023-11-12 13:32:31 +0100

    .

--- description/questions.py
+++ description/questions.py
@@ -20,10 +20,10 @@ class Query:
                 raise Exception("Invalid language")
 
     TABLES_PL = Question(
-        "Wygeneruj opis dla tabel w relacyjnej bazie danych o takiej strukturze JSON: %s Opis powinien próbować odgadnąć semantyke oraz relacje pomiędzy tabelami. Opis tabeli powinien zawierać minimalnie 3 zdania, maksymalnie 5. Wyniki zwróć w formacie JSON w postaci: {\"table_name\": \"description\"}")
+        "Wygeneruj opis dla tabel, widoków oraz funkcji w relacyjnej bazie danych o takiej strukturze JSON: %s Opis powinien próbować odgadnąć semantyke oraz relacje pomiędzy tabelami. Opis tabeli powinien zawierać minimalnie 3 zdania, maksymalnie 5. Wyniki zwróć w języku polskim w formacie JSON w postaci: { \"tables\": [{\"table_name\": \"description\"}], \"views\": [{\"view_name\": \"description\"}], \"functions\": [{\"function_name\": \"description\"}]} i niezwracaj nic więcej")
     TABLES_EN = Question(
         "Generate a description for a table with such a structure: %s The description should try to guess the semantics and relationships between tables")
 
     VIEWS_PL = Question(
-        "Wygeneruj opis dla widoków w relacyjnej bazie danych o takiej strukturze JSON: %s . Opis widoku powinien zawierać minimalnie 3 zdania, maksymalnie 5. Wyniki zwróć w formacie JSON w postaci: {\"view_name\": \"description\"}")
+        "Wygeneruj opis dla widoków w relacyjnej bazie danych o takiej strukturze JSON: %s . Opis widoku powinien zawierać minimalnie 3 zdania, maksymalnie 5. Wyniki zwróć w formacie JSON w postaci: {\"nazwa_widoku\": \"opis\"}")
     VIEWS_EN = Question("")
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import pprint
 from Kroki.kroki import get_diagram_svg, convert_svg_to_png, save_svg_diagram
 from Database import Database
+from markdown import MarkdownGenerator
 from menu import Menu
 from description import DescriptionGenerator, Query
 import logging
@@ -14,8 +15,8 @@ def run(db_url: str, lang: Query.Lang):
     db = Database()
     db.connect(db_url)
     # db.create_description_for_kroki('description_for_kroki.txt')
-    db_data = db.get_database_metadata()
-    pprint.pprint(db_data)
+    db_metadata = db.get_database_metadata()
+    pprint.pprint(db_metadata)
 
 
     svg_diagram = get_diagram_svg('description_for_kroki.txt', 'kroki.io')
@@ -23,8 +24,14 @@ def run(db_url: str, lang: Query.Lang):
     convert_svg_to_png('diagram.svg')
 
 
-    # val = DescriptionGenerator.runner(tables, views, lang)
-    # print(val)
+    # gpt_responses = {}
+    # for schema in db_metadata:
+    #     tables = db_metadata[schema]['tables']
+    #     views = db_metadata[schema]['views']
+    #     gpt_responses[schema] = DescriptionGenerator.runner(db_metadata[schema], views, lang)
+
+    # print(gpt_responses)
+    MarkdownGenerator.generate(db_metadata, {})
 
 
 if __name__ == '__main__':
